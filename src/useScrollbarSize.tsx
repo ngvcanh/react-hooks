9cfc9bdb930105286e8eb6723f3eecfedbe36bd6
Cảnh Nguyễn <canhnv2@minerva.vn>
2022-08-09 09:43:45 +0700

    Fix useScrollbarSize

--- src/useScrollbarSize.tsx
+++ src/useScrollbarSize.tsx
@@ -1,31 +1,38 @@
-import { CSSProperties, useLayoutEffect, useState } from "react";
+import { CSSProperties, useLayoutEffect } from "react";
 
 const css: CSSProperties = {
   overflow: 'scroll',
   msOverflowStyle: 'scrollbar',
-  opacity: 0,
   position: 'absolute',
-  zIndex: -1,
   pointerEvents: 'none',
-  top: '-9999px'
+  top: '-9999px',
+  width: '100px',
+  height: '100px'
 }
 
-export default function useScrollbarSize(){
+let scrollbarSize = -1;
 
-  const [ size, setSize ] = useState(0);
-
-  useLayoutEffect(() => {
-    const div = document.createElement('div');
-    Object.assign(div.style, css);
-    document.body.appendChild(div);
-
-    const inner = document.createElement('div');
-    div.appendChild(inner);
-
-    setSize(div.offsetWidth - inner.offsetWidth);
-    div.remove();
-  }, []);
+const getScrollbarSize = () => {
+  if (typeof document === "undefined") return 0;
+  const div = document.createElement('div');
+  
+  Object.assign(div.style, css);
+  document.body.appendChild(div);
+  
+  const size = div.offsetWidth - div.clientWidth;
+  document.body.removeChild(div);
 
   return size;
+}
+
+export default function useScrollbarSize(forceUpdate = false){
+
+  useLayoutEffect(() => {
+    if (!forceUpdate && scrollbarSize > 0) return;
+    scrollbarSize = getScrollbarSize();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return Math.max(scrollbarSize, 0);
 
 }
